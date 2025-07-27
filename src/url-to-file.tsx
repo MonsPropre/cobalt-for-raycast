c54ea7c79446e185108ca82f0547e36e92febc7e
LAZIC Erwan <b.baldy@proton.me>
2025-07-27 17:22:06 +0200

    Update instances.json + url tsx

--- src/url-to-file.tsx
+++ src/url-to-file.tsx
@@ -36,119 +36,140 @@ type Instance = {
 
 async function download(
     values: FormValues,
-    instance: string,
-    cobaltInstance: string,
-    cobaltInstanceUseApiKey: boolean,
-    apiKey: string
+    instance: Instance | undefined | { id: string; name: string; url: any; apiKey: any },
+    cobaltInstance: string
 ) {
-    const apiUrl = instanceUrl;
-    await showToast({
-        style: Toast.Style.Animated,
-        title: "Downloading",
-    });
+    const apiUrl = instance?.url;
+
     console.log({
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        ...(cobaltInstance === "custom" && cobaltInstanceUseApiKey ? { Authorization: `Api-Key ${apiKey}` } : {}),
-    });
-    await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            ...(customInstance && cobaltInstanceUseApiKey ? { Authorization: `Api-Key ${apiKey}` } : {}),
-        },
-        body: JSON.stringify({
-            url: values.url,
-            downloadMode: values.downloadMode,
-            filenameStyle: "nerdy",
-        }),
-        signal: AbortSignal.timeout(2500),
+        apiUrl,
+        cobaltInstance,
+        apiKey: instance?.apiKey
     })
-        .then(async (response) => {
-            const resJson = await response.json();
-            if (!response.ok) {
-                throw resJson;
-            }
-            return resJson;
-        })
-        .then(async (data) => {
-            if (data && data.status === "tunnel") {
-                await showToast({
-                    style: Toast.Style.Animated,
-                    title: "Tunnel created",
-                    message: `Tunnel ${new URL(data.url).searchParams.get("id")} created.`,
-                });
-                await confirmAlert({
-                    title: `Download ${data?.filename} ?`,
-                    primaryAction: {
-                        title: "Download",
-                        onAction: async () => {
-                            await showToast({
-                                style: Toast.Style.Success,
-                                title: "Download started",
-                            });
-                            await popToRoot();
-                            await open(data.url);
-                        },
-                    },
-                    dismissAction: {
-                        title: "Cancel",
-                        onAction: async () => {
-                            await showToast({
-                                style: Toast.Style.Failure,
-                                title: "Download canceled",
-                            });
-                        },
-                    },
-                });
-            }
-        })
-        .catch(async (error) => {
-            if (error?.status === "error") {
-                await showToast({
-                    style: Toast.Style.Failure,
-                    title: "Failed to download",
-                    message: getErrorMessage(error?.error?.code),
-                });
-                return;
-            }
-            if (error?.name) {
-                await showToast({
-                    style: Toast.Style.Failure,
-                    title: "Failed to download",
-                    message: getErrorMessage(error?.error?.code),
-                });
-                return;
-            }
-            console.error({
-                name: error.name,
-            });
-            await showToast({
-                style: Toast.Style.Failure,
-                title: error?.name || "Failed to download",
-                message: getErrorMessage(error?.message),
-            });
-        });
+
+    // await showToast({
+    //     style: Toast.Style.Animated,
+    //     title: "Downloading",
+    // });
+    // console.log({
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //     ...(cobaltInstance === "custom" && cobaltInstanceUseApiKey ? { Authorization: `Api-Key ${apiKey}` } : {}),
+    // });
+    // await fetch(apiUrl, {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         Accept: "application/json",
+    //         ...(instance?.apiKey !== null ? { Authorization: `Api-Key ${apiKey}` } : {}),
+    //     },
+    //     body: JSON.stringify({
+    //         url: values.url,
+    //         downloadMode: values.downloadMode,
+    //         filenameStyle: "nerdy",
+    //     }),
+    //     signal: AbortSignal.timeout(2500),
+    // })
+    //     .then(async (response) => {
+    //         const resJson = await response.json();
+    //         if (!response.ok) {
+    //             throw resJson;
+    //         }
+    //         return resJson;
+    //     })
+    //     .then(async (data) => {
+    //         if (data && data.status === "tunnel") {
+    //             await showToast({
+    //                 style: Toast.Style.Animated,
+    //                 title: "Tunnel created",
+    //                 message: `Tunnel ${new URL(data.url).searchParams.get("id")} created.`,
+    //             });
+    //             await confirmAlert({
+    //                 title: `Download ${data?.filename} ?`,
+    //                 primaryAction: {
+    //                     title: "Download",
+    //                     onAction: async () => {
+    //                         await showToast({
+    //                             style: Toast.Style.Success,
+    //                             title: "Download started",
+    //                         });
+    //                         await popToRoot();
+    //                         await open(data.url);
+    //                     },
+    //                 },
+    //                 dismissAction: {
+    //                     title: "Cancel",
+    //                     onAction: async () => {
+    //                         await showToast({
+    //                             style: Toast.Style.Failure,
+    //                             title: "Download canceled",
+    //                         });
+    //                     },
+    //                 },
+    //             });
+    //         }
+    //     })
+    //     .catch(async (error) => {
+    //         if (error?.status === "error") {
+    //             await showToast({
+    //                 style: Toast.Style.Failure,
+    //                 title: "Failed to download",
+    //                 message: getErrorMessage(error?.error?.code),
+    //             });
+    //             return;
+    //         }
+    //         if (error?.name) {
+    //             await showToast({
+    //                 style: Toast.Style.Failure,
+    //                 title: "Failed to download",
+    //                 message: getErrorMessage(error?.error?.code),
+    //             });
+    //             return;
+    //         }
+    //         console.error({
+    //             name: error.name,
+    //         });
+    //         await showToast({
+    //             style: Toast.Style.Failure,
+    //             title: error?.name || "Failed to download",
+    //             message: getErrorMessage(error?.message),
+    //         });
+    //     });
 }
 
 export default function Command() {
     const { cobaltInstance, cobaltInstanceUrl, cobaltInstanceUseApiKey, cobaltInstanceApiKey } = getPreferenceValues();
 
     const { isLoading, data } = useFetch<Instance[]>(
-        "https://raw.githubusercontent.com/MonsPropre/cobalt-for-raycast/refs/heads/main/assets/instances.json"
+        "https://raw.githubusercontent.com/monspropre/cobalt-for-raycast/main/assets/instances.json"
     );
+
+    // Parse 'data' string JSON if needed
+    let instances: Instance[] = [];
+    if (typeof data === "string") {
+        try {
+            instances = JSON.parse(data);
+        } catch (error) {
+            instances = [];
+            console.error("Failed to parse instances JSON:", error);
+        }
+    } else if (Array.isArray(data)) {
+        instances = data;
+    }
 
     const { handleSubmit, itemProps, reset } = useForm<FormValues>({
         async onSubmit(values) {
-            // await download(
-            //     values,
-            //     cobaltInstance === "public-instance" ? values.instance : cobaltInstanceUrl,
-            //     cobaltInstance,
-            //     cobaltInstanceUseApiKey,
-            //     cobaltInstanceApiKey
-            // );
-            console.log(values);
+            const cobaltCustomInstance = {
+                id: "custom",
+                name: "Custom",
+                url: cobaltInstanceUrl,
+                apiKey: Boolean(cobaltInstanceUseApiKey) ? cobaltInstanceApiKey : undefined
+            }
+            await download(
+                values,
+                cobaltInstance === "public-instance" ? instances.find((instance) => instance.id === values.instance) : cobaltCustomInstance,
+                cobaltInstance
+            );
         },
         validation: {
             url: (value) => {
@@ -166,19 +187,6 @@ export default function Command() {
         },
     });
 
-    // Parse 'data' string JSON if needed
-    let instances: Instance[] = [];
-    if (typeof data === "string") {
-        try {
-            instances = JSON.parse(data);
-        } catch (error) {
-            instances = [];
-            console.error("Failed to parse instances JSON:", error);
-        }
-    } else if (Array.isArray(data)) {
-        instances = data;
-    }
-
     useEffect(() => {
         (async () => {
             const text = await Clipboard.readText();
@@ -192,10 +200,6 @@ export default function Command() {
             }
         })();
     }, [reset]);
-
-    if (isLoading) {
-        return <Form isLoading />;
-    }
 
     return (
         <Form
