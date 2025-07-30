4b5fa33fce9025b588dde309a533af314b2cb00d
LAZIC Erwan <b.baldy@proton.me>
2025-07-30 02:28:03 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,3 @@
 module.exports = {
 	extends: ["@raycast"],
-	rules: {
-		// // You can customize rules here
-		// "@typescript-eslint/no-explicit-any": "off",
-		// "@typescript-eslint/no-unused-vars": "off"
-	}
 };
